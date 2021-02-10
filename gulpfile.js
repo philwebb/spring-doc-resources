9566cb75271d0fe66daa70869d04a034a197f074
Phillip Webb <pwebb@vmware.com>
2021-02-10 15:17:59 -0800

    Hacking

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,20 @@
 
 const { src, dest, series, parallel, watch } = require('gulp');
 const asciidoctor = require('@asciidoctor/gulp-asciidoctor');
+const browserify = require('browserify');
 const connect = require('gulp-connect');
+const log = require('gulplog');
+const tap = require('gulp-tap');
+const buffer = require('gulp-buffer');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
 
 const paths = {
     dist: 'build/dist/',
     web: 'build/web/'
 };
 
-const asciidoctorAttributes = [
+const ASCIIDOC_ATTRIBUTES = [
     'icons=font',
     'idprefix',
     'idseparator=-',
@@ -26,12 +32,25 @@ const asciidoctorAttributes = [
     'docinfodir='.concat(process.cwd(), "/", paths.dist)
 ]
 
+function js() {
+    return src('src/main/js/*.js', {read: false})
+        .pipe(tap(function (file) {
+            log.info('bundling ' + file.path);
+            file.contents = browserify(file.path, {debug: true}).bundle();
+        }))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dest'));
+}
+
 function renderAsciidoctorExample() {
     return src('src/test/docs/asciidoc/index.adoc')
         .pipe(asciidoctor({
             safe: 'unsafe', 
             doctype: 'book', 
-            attributes: asciidoctorAttributes
+            attributes: ASCIIDOC_ATTRIBUTES
         }))
         .pipe(dest(paths.web))
         .pipe(connect.reload());
@@ -53,3 +72,4 @@ function watchFiles(cb) {
 const update = series(renderAsciidoctorExample);
 
 exports.dev = series(update, parallel(webServer, watchFiles));
+exports.js = js;
---
+++ src/main/js/highlight.bundle.js
@@ -0,0 +1,28 @@
+;(function () {
+  'use strict'
+
+  var hljs = require('highlight.js/lib/highlight')
+  hljs.registerLanguage('asciidoc', require('highlight.js/lib/languages/asciidoc'))
+  hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'))
+  hljs.registerLanguage('css', require('highlight.js/lib/languages/css'))
+  hljs.registerLanguage('diff', require('highlight.js/lib/languages/diff'))
+  hljs.registerLanguage('dockerfile', require('highlight.js/lib/languages/dockerfile'))
+  hljs.registerLanguage('gradle', require('highlight.js/lib/languages/gradle'))
+  hljs.registerLanguage('groovy', require('highlight.js/lib/languages/groovy'))
+  hljs.registerLanguage('http', require('highlight.js/lib/languages/http'))
+  hljs.registerLanguage('java', require('highlight.js/lib/languages/java'))
+  hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'))
+  hljs.registerLanguage('json', require('highlight.js/lib/languages/json'))
+  hljs.registerLanguage('kotlin', require('highlight.js/lib/languages/kotlin'))
+  hljs.registerLanguage('markdown', require('highlight.js/lib/languages/markdown'))
+  hljs.registerLanguage('nix', require('highlight.js/lib/languages/nix'))
+  hljs.registerLanguage('properties', require('highlight.js/lib/languages/properties'))
+  hljs.registerLanguage('scala', require('highlight.js/lib/languages/scala'))
+  hljs.registerLanguage('shell', require('highlight.js/lib/languages/shell'))
+  hljs.registerLanguage('sql', require('highlight.js/lib/languages/sql'))
+  hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
+  hljs.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'))
+  ;[].slice.call(document.querySelectorAll('pre code.hljs')).forEach(function (node) {
+    hljs.highlightBlock(node)
+  })
+})()
